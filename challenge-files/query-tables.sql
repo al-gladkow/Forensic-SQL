f6adb904555316983fb3b2bfd4bd3934349472d5
al-gladkow <103766014+kimjongal@users.noreply.github.com>
2022-07-30 04:09:05 -0500

    Completed the first few queries

--- challenge-files/query-tables.sql
+++ challenge-files/query-tables.sql
@@ -1,109 +1,51 @@
-/*select id
-from card_holder
-where id in (
-	
-	select cardholder_id 
-	from credit_card
-	where card in (
-	
-		select transaction.card from
-	
-	)
-);
-group by card_holder.id;*/
+---------------------------------------------------------
+------------ Create view for joined tables --------------
+---------------------------------------------------------
+create view txn_list as
+select txn_id, amount, txns_noname.card, ch.name, ch_id, id_merchant
+from (
+	select id txn_id, amount, t.card, cardholder_id ch_id, id_merchant
+	from "transaction" t
+	join credit_card cc
+	on t.card = cc.card
+) txns_noname
+join card_holder ch 
+on txns_noname.ch_id = ch.id
 
+---------------------------------------------------------
+-- Lists people with the most transactions less than $2 -
+---------------------------------------------------------
+create view small_txns as
 
+select  ch_id, name, count(txn_id) num_txns
+from txn_list
+where txn_list.amount < 2.00
+group by name, ch_id
+order by num_txns desc;
 
-/*select count(id)
-from t
-where id in (
+---------------------------------------------------------
+---- Lists transactions made between 07:00 and 09:00 ----
+--------------------------------------------------------- 
+create view early_txns as
 
-	select id
-	from "transaction" t 
-	where t.amount < 2
-
-);*/
-
-
-/*
---
-select count(id), id
-from "transaction" t 
-where t.amount < 2
-group by id;*/
-
-/*-- How many cards does each card holder have?
-select cardholder_id, count(card)
-from  credit_card cc 
-group by cardholder_id;
-
-
--- Transaction IDs for transactions less than $2.00
-select id
-from "transaction" t 
-where amount < 2;*/
-
-/*select cardholder_id as holder_id
-from card_holder ch
-where holder_id in (
-
-	select card, count(id) as "potential fraud txns"
-	from "transaction"
-	where amount < 2.00
-	group by card
-	order by count(id) desc
-
-);*/
-
-/*select card, count(id) as "#txns"
-from "transaction"
-where amount < 2.00
-group by card;*/
-
-
-/*select card
-from "transaction" t 
-where amount < 2.00*/
-
-/*select credit_card.cardholder_id as ccid
-from credit_card
-where card in (
-	select card
-	from "transaction" t 
-	where amount < 2.00
-)
-group by ccid*/
-
-/*	select card, id, amount
-	from "transaction"
-	where amount < 2.00*/
-
-
-/*where amount < 2.00
-group by card
-order by count(id) desc
-
-select cardholder_id, txns.card
-from credit_card cc
-full outer join(
-	select card, count(id) as "#txns"
-	from "transaction"
-	where amount < 2.00
-	group by card
-	order by count(id) desc
-) txns
-on cc.card = txns.card
-group by cardholder_id;*/
-
-
-select id as "txn_id", "date", amount, t.card, cardholder_id
+select id, "date"::time
 from "transaction" t
-inner join credit_card cc
-on t.card = cc.card
+where date::time >= '07:00:00'::time
+and date::time < '09:00:00'::time
+
+---------------------------------------------------------
+-------------- Lists anomalous transactions -------------
+---------------------------------------------------------
 
 
+---------------------------------------------------------
+--- Are there more anomalous txns between 7am and 9am? --
+--------------------------------------------------------- 
 
 
+---------------------------------------------------------
+------------ Top 5 merchants prone to hacking -----------
+--------------------------------------------------------- 
 
 
 
